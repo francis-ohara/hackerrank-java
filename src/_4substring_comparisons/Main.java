f50f737f5adc91eb9802ab7967ff0031ec1b143c
Francis O'Hara Aidoo <kofiohara@gmail.com>
2022-05-14 02:13:01 +0000

    java-substring-comparisons

---
+++ src/_4substring_comparisons/Main.java
@@ -0,0 +1,28 @@
+package _4substring_comparisons;
+
+import java.util.ArrayList;
+public class Main {
+
+    public static void main(String[] args){
+
+        String s = "welcometojava";
+        int k = 3;
+        ArrayList<String> substrings = new ArrayList<>();
+
+        for(int i = 0; i<= s.length()-k; i++)
+            substrings.add(s.substring(i, i+k));
+
+        String largest = substrings.get(0);
+        String smallest = substrings.get(0);
+
+        for (String substring : substrings) {
+            if (largest.compareTo(substring) < 0)
+                largest = substring;
+            if (smallest.compareTo(substring) > 0)
+                smallest = substring;
+        }
+
+        System.out.println(smallest + "\n" + largest);
+
+    }
+}
