295b568314c30a3821551663220137c51f64a770
Francis O'Hara Aidoo <kofiohara@gmail.com>
2022-05-16 12:04:27 +0000

    java-string-tokens

---
+++ src/_7string_tokens/Main.java
@@ -0,0 +1,32 @@
+package _7string_tokens;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine() + " ";
+        int n;
+        String temp = "";
+        ArrayList<String> result = new ArrayList<>();
+
+        for(char ch: s.toCharArray()) {
+            if(ch!=' ' && ch!='!' && ch!=',' && ch!='?' && ch!='@' && ch!='_' && ch!='\'' && ch!='.') {
+                temp += ch;
+            }
+            else if(temp.length()>0) {
+                result.add(temp);
+                temp = "";
+            }
+        }
+
+        n = result.size();
+        System.out.println(n);
+        for(String string: result)
+            System.out.println(string);
+
+
+
+    }
+}
