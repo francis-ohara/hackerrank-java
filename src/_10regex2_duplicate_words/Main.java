cf0b3a78f7c2a92af14337c7355465617675b3fc
Francis O'Hara Aidoo <kofiohara@gmail.com>
2022-05-23 14:28:50 +0000

    java-regex-2-duplicate-words-initial-commit

---
+++ src/_10regex2_duplicate_words/Main.java
@@ -0,0 +1,7 @@
+package _10regex2_duplicate_words;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
--- src/_8pattern_syntax_checker/Main.java
+++ src/_8pattern_syntax_checker/Main.java
@@ -1,23 +1,9 @@
 package _8pattern_syntax_checker;
 
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        input.nextLine();
-        for (int i = 0; i < N; i++) {
-            try {
-                String string = input.nextLine();
-                System.out.println(string);
-                Pattern.compile(string);
-                System.out.println("Valid");
-            } catch (PatternSyntaxException e) {
-                System.out.println("Invalid");
-            }
-        }
+    public static void main(String[] args){
+
+
     }
 }
